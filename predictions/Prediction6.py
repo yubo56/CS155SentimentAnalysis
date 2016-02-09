2df36294ce81a122b06c356bc06530983b6960cd
Yubo Su <yubo56@gmail.com>
2016-02-09 02:37:42 -0800

    Parallelized Prediction6

--- predictions/Prediction6.py
+++ predictions/Prediction6.py
@@ -13,6 +13,7 @@ import sklearn.linear_model as lm
 import sklearn.feature_extraction.text as tfidf
 
 if __name__ == '__main__':
+    min_changes = 5
     trainx, trainy = ld.loadtrain('data/trainingdata.txt')
     testx = ld.loadtest('data/testingdata.txt')
     trainx = tfidf.TfidfTransformer().fit_transform(trainx).toarray()
@@ -22,14 +23,14 @@ if __name__ == '__main__':
 
     # loop until few changes
     numChange = len(testx)
-    while numChange > 5:
+    while numChange > min_changes:
         rand = randint(0, 8)
         if rand == 0:
             numChange = comp.append(ens.AdaBoostClassifier(n_estimators=100,
                 learning_rate=0.03))
         elif rand == 1:
             numChange = comp.append(ens.BaggingClassifier(n_estimators=100,
-                max_features=0.5, max_samples=0.7))
+                max_features=0.5, max_samples=0.7, n_jobs=-1))
         elif rand == 2:
             numChange =\
             comp.append(tree.DecisionTreeClassifier(max_features=0.64,
@@ -40,7 +41,7 @@ if __name__ == '__main__':
                 min_samples_split=7, max_depth=6, subsample=0.7))
         elif rand == 4:
             numChange = comp.append(ens.RandomForestClassifier(n_estimators=450,
-                max_features=0.0420, min_samples_split=7))
+                max_features=0.0420, min_samples_split=7, njobs=-1))
         elif rand == 5:
             numChange = comp.append(lm.SGDClassifier(loss='hinge',
                 alpha=0.000631))
@@ -48,7 +49,7 @@ if __name__ == '__main__':
             numChange = comp.append(svm.LinearSVC(dual=False, C=0.01))
         elif rand == 7:
             numChange = comp.append(ens.RandomForestClassifier(n_estimators=100,
-                max_features=0.320, min_samples_split=7))
+                max_features=0.320, min_samples_split=7, n_jobs=-1))
         elif rand == 8:
             numChange = comp.append(svm.LinearSVC(dual=False, C=1000))
         ld.write('predictions/Prediction6.txt', comp.predict())

6b290b433f0f9c34ab9a0f88d70d074a9990de09
Yubo Su <yubo56@gmail.com>
2016-02-09 10:01:01 -0800

    Set up linear-time CV appending to composite

--- lib/composite.py
+++ lib/composite.py
@@ -48,3 +48,90 @@ class Composite(object):
         scores self on new dataset
         """
         return np.mean([i.score(datax, datay) for i in self.clfs])
+
+
+# this should really extend Composite but I'm lazy
+class CompositeCV(object):
+    """
+    Composite predictor
+
+    Only adds if improves cv score
+    """
+    def __init__(self, trainx, trainy, testx, cv=5):
+        super(CompositeCV, self).__init__()
+        assert len(trainx) == len(trainy)
+        assert len(trainx[0]) == len(testx[0])
+        self.__trainx = trainx
+        self.__trainy = trainy
+        self.__testx = testx
+        self.__preds = np.array([0] * len(testx))       # store private copy
+        self.clfs = list()
+        self.preds = np.array([0] * len(testx))         # public copy, in [0,1]
+
+        # set up to do cv
+        self.__cv = list()
+        lendat = len(self.__trainx)
+        for i in range(cv):
+            trnx = self.__trainx[lendat // cv * 0: lendat // cv * i]
+            trnx = np.concatenate((trnx, 
+                self.__trainx[lendat // cv * (i+1):]))
+            trny = self.__trainy[lendat // cv * 0: lendat // cv * i]
+            trny = np.concatenate((trny, 
+                self.__trainy[lendat // cv * (i+1):]))
+            valx = self.__trainx[lendat // cv * i: lendat // cv * (i+1)]
+            self.__cv.append((trnx, trny, valx))
+        self.__cvpreds = np.array([0] * (lendat // cv * cv)) # cv predictions
+
+    @staticmethod
+    def diff(x, y):
+        """
+        helper, counts differences between x, y
+        truncates to len x
+        """
+        return sum([x[i] != y[i] for i in range(len(x))])
+
+    def append(self, clf):
+        """
+        trains and updates score
+        appends model to Composite
+        :return: agreement with __trainy of cv
+        """
+        print("\tNumber classifiers: " + str(len(self.clfs)) + "\t Last type: "
+                + type(clf).__name__)
+        cvpreds = list()            # store current predictions
+        # generate cv predictions
+        for i in range(len(self.__cv)):
+            clf.fit(self.__cv[i][0], self.__cv[i][1])
+            cvpreds.extend(clf.predict(self.__cv[i][2]))
+        cvpreds = np.array(cvpreds).astype(int) # allow vectorizing
+        # check whether cv improves
+        old_preds = self.__cvpreds\
+                // (len(self.clfs) // 2 + 1) # old cv preds
+        new_preds = (self.__cvpreds + cvpreds)\
+                // ((len(self.clfs) + 1) // 2 + 1) # new cv preds
+        if self.diff(new_preds, self.__trainy) < \
+                self.diff(old_preds, self.__trainy): # if num differences decreases
+            # change cv, refit and append
+            self.__cvpreds += cvpreds
+            clf.fit(self.__trainx, self.__trainy)
+            self.clfs.append(clf)
+
+            self.__preds += clf.predict(self.__testx)
+            self.preds = self.__preds // (len(self.clfs) // 2 + 1)
+
+            return self.diff(new_preds, self.__trainy) # return cv agreement
+        else:
+            return -1 # do not add
+
+    def predict(self):
+        """
+        returns own prediction
+        """
+        assert min(self.preds) >= 0 and max(self.preds) <= 1
+        return self.preds # returns 1 where majority said 1
+    
+    def score(self, datax, datay):
+        """
+        scores self on new dataset
+        """
+        return np.mean([i.score(datax, datay) for i in self.clfs])
--- predictions/Prediction7.py
+++ predictions/Prediction7.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# loop until num_consec zeroes
 import numpy as np
 from pickle import Pickler
 import random as r
@@ -13,17 +14,21 @@ import sklearn.linear_model as lm
 import sklearn.feature_extraction.text as tfidf
 
 if __name__ == '__main__':
-    min_changes = 3
+    num_consec = 3              # terminate after this many 0 returns
+    n_jobs = 1                  # set njobs= this on parallel computers
+    save_interval = 5            # how many trials to save on
+
+    curr_consec = 0
     trainx, trainy = ld.loadtrain('data/trainingdata.txt')
     testx = ld.loadtest('data/testingdata.txt')
     trainx = tfidf.TfidfTransformer().fit_transform(trainx).toarray()
     testx = tfidf.TfidfTransformer().fit_transform(testx).toarray()
-    comp = c.Composite(trainx, trainy, testx)
+    comp = c.CompositeCV(trainx, trainy, testx)
     p = Pickler(open('predictions/comp.learn', 'wb'), -1)
 
     # loop until few changes
     numChange = len(testx)
-    while True:
+    while curr_consec <= num_consec:
         rand = r.randint(0, 8)
         if rand == 0:
             numChange = comp.append(ens.AdaBoostClassifier(n_estimators=100,
@@ -31,7 +36,7 @@ if __name__ == '__main__':
         elif rand == 1:
             numChange = comp.append(ens.BaggingClassifier(n_estimators=100,
                 max_features=0.3 + 0.4 * r.random(), max_samples=0.5 + 0.4 *
-                r.random(), n_jobs=-1))
+                r.random(), n_jobs=n_jobs))
                 # 0.5 +- 0.2, 0.7 +- 0.2
         elif rand == 2:
             numChange =\
@@ -47,7 +52,7 @@ if __name__ == '__main__':
         elif rand == 4:
             numChange = comp.append(ens.RandomForestClassifier(n_estimators=450,
                 max_features=0.0020 + 0.2 * r.random(), min_samples_split=7,
-                n_jobs=-1))
+                n_jobs=n_jobs))
                 # 0.0420 + 0.16 - 0.04
         elif rand == 5:
             numChange = comp.append(lm.SGDClassifier(loss='hinge',
@@ -60,15 +65,17 @@ if __name__ == '__main__':
         elif rand == 7:
             numChange = comp.append(ens.RandomForestClassifier(n_estimators=100,
                 max_features=0.120 + 0.4 * r.random(), min_samples_split=7,
-                n_jobs=-1))
+                n_jobs=n_jobs))
                 # 0.32 +- 0.2
         elif rand == 8:
             numChange = comp.append(svm.LinearSVC(dual=False, C=100 + 900 *
                 r.random()))
                 # 1000 - 900
-        ld.write('predictions/Prediction6.txt', comp.predict())
-        print('Number Changes: ' + str(numChange))
-        p.dump(comp.clfs)
-
-# 2289 added to comp before out of memory
-# didn't pickle properly, haven't rerun, ref. Prediction7.py
+        if numChange == 0:
+            num_consec += 1
+        else:
+            num_consec = 0
+        print('Number CV Changes: ' + str(numChange))
+        if len(comp.clfs) % save_interval == 0: # don't save every time
+            ld.write('predictions/Prediction7.txt', comp.predict())
+            p.dump(comp.clfs)
